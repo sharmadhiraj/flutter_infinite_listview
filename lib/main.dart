4bc534ed7af689bb322861ede2c21ff2edbb79c0
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2020-04-27 15:59:17 +0545

    Implement infinte scrolling with ListViewBuilder.

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,170 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Infinite Scrolling ListView",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PhotosListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class PhotosListScreen extends StatefulWidget {
+  PhotosListScreen({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _PhotosListScreenState createState() => _PhotosListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PhotosListScreenState extends State<PhotosListScreen> {
+  bool _hasMore;
+  int _pageNumber;
+  bool _error;
+  bool _loading;
+  final int _defaultPhotosPerPageCount = 10;
+  List<Photo> _photos;
+  final int _nextPageThreshold = 5;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _hasMore = true;
+    _pageNumber = 1;
+    _error = false;
+    _loading = true;
+    _photos = [];
+    fetchPhotos();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: Text("Photos App")),
+      body: getBody(),
     );
+  }
+
+  Widget getBody() {
+    if (_photos.isEmpty) {
+      if (_loading) {
+        return Center(
+            child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: CircularProgressIndicator(),
+        ));
+      } else if (_error) {
+        return Center(
+            child: InkWell(
+          onTap: () {
+            setState(() {
+              _loading = true;
+              _error = false;
+              fetchPhotos();
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text("Error while loading photos, tap to try agin"),
+          ),
+        ));
+      }
+    } else {
+      return ListView.builder(
+          itemCount: _photos.length + (_hasMore ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (index == _photos.length - _nextPageThreshold) {
+              fetchPhotos();
+            }
+            if (index == _photos.length) {
+              if (_error) {
+                return Center(
+                    child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      _loading = true;
+                      _error = false;
+                      fetchPhotos();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text("Error while loading photos, tap to try agin"),
+                  ),
+                ));
+              } else {
+                return Center(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: CircularProgressIndicator(),
+                ));
+              }
+            }
+            final Photo photo = _photos[index];
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  Image.network(
+                    photo.thumbnailUrl,
+                    fit: BoxFit.fitWidth,
+                    width: double.infinity,
+                    height: 160,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(photo.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)),
+                  ),
+                ],
+              ),
+            );
+          });
+    }
+    return Container();
+  }
+
+  Future<void> fetchPhotos() async {
+    try {
+      final response = await http.get(
+          "https://jsonplaceholder.typicode.com/photos?_page=$_pageNumber");
+      List<Photo> fetchedPhotos = Photo.parseList(json.decode(response.body));
+      setState(() {
+        _hasMore = fetchedPhotos.length == _defaultPhotosPerPageCount;
+        _loading = false;
+        _pageNumber = _pageNumber + 1;
+        _photos.addAll(fetchedPhotos);
+      });
+    } catch (e) {
+      setState(() {
+        _loading = false;
+        _error = true;
+      });
+    }
+  }
+}
+
+class Photo {
+  final String title;
+  final String thumbnailUrl;
+
+  Photo(this.title, this.thumbnailUrl);
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(json["title"], json["thumbnailUrl"]);
+  }
+
+  static List<Photo> parseList(List<dynamic> list) {
+    return list.map((i) => Photo.fromJson(i)).toList();
   }
 }
